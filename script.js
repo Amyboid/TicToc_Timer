3a74e3553d9ac4243f47aa3386f743707772d544
Amyboid <134444582+Amyboid@users.noreply.github.com>
2025-04-09 01:13:44 +0530

    updated logic & fucked up styles

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ function addNewCounter(event) {
     // Get the value from the input field
     let dateValue = document.getElementById("date").value;
     let showMsg = document.getElementById("show");
-    showMsg.innerText = "";
+    // let sshowMsgStyles = window.getComputedStyle(showMsg)
+    showMsg.style.opacity = 0
     // console.log(dateValue);
 
     // Create a new Date object from the input value
@@ -31,7 +32,8 @@ function addNewCounter(event) {
     }
 
     if (difference < 0) {
-        showMsg.innerText = "We can't go back to past";
+        // showMsg.innerText = "We can't go back to past";
+        showMsg.style.opacity = 1
         return;
     }
 
@@ -48,26 +50,40 @@ function addNewCounter(event) {
 
 function saveCountdownToLocalStorage(countdownData) {
     if (localStorage.getItem("stateArray")) {
-        let stateArray = JSON.parse(localStorage.getItem("stateArray")) || [];
+        let stateArray = JSON.parse(localStorage.getItem("stateArray"));
+        console.log('state array if: ', stateArray, countdownData)
         stateArray.push(countdownData);
         localStorage.setItem("stateArray", JSON.stringify(stateArray));
     } else {
+        console.log('state array else: ', countdownData)
         localStorage.setItem("stateArray", JSON.stringify([countdownData]));
     }
 }
-function createCountdownDisplay(countdownData) {
-    let allData = JSON.parse(localStorage.getItem("stateArray")) || [];
-    console.log(allData);
 
-    let currentCountdown = allData.find(
-        (data) => data.id === countdownData.id
+function deleteCoundownDisplay(countDownId, updateUi) {
+    const data = JSON.parse(localStorage.getItem("stateArray"))
+    let newStateArray = data.filter(
+        (data) => data.id !== countDownId
     );
-    console.log(currentCountdown.id);
-    let inputDate = new Date(currentCountdown.inputDate);
-    // Create a new countdown display
-    let newCountDown = document.createElement("div");
-    newCountDown.className = "countdown-display";
+    localStorage.setItem("stateArray", JSON.stringify(newStateArray));
+    if (updateUi) {
+        document.getElementById('countdown-container').innerHTML = ''
+        loadCountdownsFromLocalStorage();
+    }
+}
 
+function createCountdownDisplay(currentCountdown) {
+
+    let inputDate = new Date(currentCountdown.inputDate);
+
+    // Create a new countdown display with noise
+    let countdownDisplay = document.createElement("div");
+    let noise = document.createElement("div");
+
+    countdownDisplay.className = "countdown-display";
+    noise.className = 'noise';
+
+    countdownDisplay.appendChild(noise);
     let successMsg = document.createElement("div");
     // creating pie spinner markup
     let wrapper = document.createElement("div");
@@ -75,45 +91,48 @@ function createCountdownDisplay(countdownData) {
     let filler = document.createElement("div");
     let mask = document.createElement("div");
 
-
-    // let progressDiv = document.createElement("div");
-    // let progressBar = document.createElement("div");
-    // progressDiv.className = "progress-div";
-    // progressBar.className = "progress-bar";
     successMsg.className = "success-msg";
     wrapper.className = "wrapper"
     spinner.className = "pie spinner"
     filler.className = "pie filler"
     mask.className = "mask"
- 
+
     wrapper.appendChild(spinner)
     wrapper.appendChild(filler)
     wrapper.appendChild(mask)
 
-    // progressDiv.appendChild(progressBar);
-    // successMsg.appendChild(progressDiv);
-    successMsg.appendChild(wrapper);
+    // creating delete display button 
+    const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M6 18V6h12v12z"/></svg>`
 
-    // Create time units
+    let deleteDisplayButton = document.createElement("button")
+    deleteDisplayButton.innerHTML = deleteIcon;
+    deleteDisplayButton.addEventListener('click', () => deleteCoundownDisplay(currentCountdown.id, true));
+    deleteDisplayButton.className = 'delete-display-btn'
+
+    successMsg.appendChild(wrapper);
+    successMsg.appendChild(deleteDisplayButton);
+
     let timeUnits = ["day", "hour", "minute", "second"];
     timeUnits.forEach((unit) => {
-        let div = document.createElement("div");
+        let unitDisplay = document.createElement("div");
         let span = document.createElement("span");
         let h4 = document.createElement("h4");
-        div.className = 'unit-display';
-        span.className = unit;
+        // adding class names 
+        unitDisplay.className = 'unit-display';
         span.innerText = "00";
+        span.className = unit
         h4.innerText = unit.charAt(0).toUpperCase() + unit.slice(1);
-        div.appendChild(span);
-        div.appendChild(h4);
-        newCountDown.appendChild(div);
-        newCountDown.appendChild(successMsg);
+        unitDisplay.appendChild(span);
+        unitDisplay.appendChild(h4);
+
+        noise.appendChild(unitDisplay)
+        noise.appendChild(successMsg);
     });
 
     // append new countdown to the container
     document
         .getElementById("countdown-container")
-        .appendChild(newCountDown);
+        .appendChild(countdownDisplay);
 
     // Calculate the difference in milliseconds
     let currentDate = new Date();
@@ -125,22 +144,19 @@ function createCountdownDisplay(countdownData) {
         let difference = inputDate - currentDate;
         let percentage = parseFloat((difference / goal) * 100);
 
-        // Update the progress bar width
-        // progressBar.style.width = percentage + "%";
-
         // Update the spinner rotation based on the percentage
-        const rotation = - (percentage / 100) * 360; 
+        const rotation = - (percentage / 100) * 360;
         let maskStyle = window.getComputedStyle(mask)
         // console.log(maskStyle.opacity, typeof(maskStyle.opacity))
-        if (rotation > -180 && parseInt(maskStyle.opacity) === 1) { 
-            console.log(rotation); 
+        if (rotation > -180 && parseInt(maskStyle.opacity) === 1) {
+            console.log(rotation);
             // remove the mask
             mask.style.opacity = 0
             // add the filler
             filler.style.opacity = 1
         }
-        
-        spinner.style.transform = `rotate(${rotation}deg)`; 
+
+        spinner.style.transform = `rotate(${rotation}deg)`;
 
         if (difference > 0) {
             let days = Math.floor(difference / (1000 * 60 * 60 * 24));
@@ -153,68 +169,32 @@ function createCountdownDisplay(countdownData) {
             let seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
             // Update the countdown display
-            newCountDown.querySelector(".day").innerText = formatTimeUnit(days);
-            newCountDown.querySelector(".hour").innerText =
+            countdownDisplay.querySelector(".day").innerText = formatTimeUnit(days);
+            countdownDisplay.querySelector(".hour").innerText =
                 formatTimeUnit(hours);
-            newCountDown.querySelector(".minute").innerText =
+            countdownDisplay.querySelector(".minute").innerText =
                 formatTimeUnit(minutes);
-            newCountDown.querySelector(".second").innerText =
+            countdownDisplay.querySelector(".second").innerText =
                 formatTimeUnit(seconds);
         } else {
             clearInterval(intervalId);
-            let newStateArray = allData.filter(
-                (data) => data.id != countdownData.id
-            );
-            localStorage.setItem("stateArray", JSON.stringify(newStateArray));
+            deleteCoundownDisplay(currentCountdown.id, false);
             successMsg.innerText = "🎉";
         }
     }, 1000);
-
-
-    // const intervalId = setInterval(() => {
-    //     let currentDate = new Date();
-    //     let difference = inputDate - currentDate;
-    //     let percentage = parseFloat((difference / goal) * 100);
-    //     // console.log(percentage);
-    //     progressBar.style.width = percentage + "%";
-    //     if (difference > 0) {
-    //         let days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    //         let hours = Math.floor(
-    //             (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    //         );
-    //         let minutes = Math.floor(
-    //             (difference % (1000 * 60 * 60)) / (1000 * 60)
-    //         );
-    //         let seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-    //         // Update the countdown display
-    //         newCountDown.querySelector(".day").innerText = formatTimeUnit(days);
-    //         newCountDown.querySelector(".hour").innerText =
-    //             formatTimeUnit(hours);
-    //         newCountDown.querySelector(".minute").innerText =
-    //             formatTimeUnit(minutes);
-    //         newCountDown.querySelector(".second").innerText =
-    //             formatTimeUnit(seconds);
-    //     } else {
-    //         clearInterval(intervalId);
-    //         let newStateArray = allData.filter(
-    //             (data) => data.id != countdownData.id
-    //         );
-    //         localStorage.setItem("stateArray", JSON.stringify(newStateArray));
-    //         successMsg.innerText = "Time's Up!";
-    //     }
-    // }, 1000);
 }
 
 function loadCountdownsFromLocalStorage() {
-    let allData = JSON.parse(localStorage.getItem("stateArray")) || [];
-    allData.forEach((countdownData) => {
-        createCountdownDisplay(countdownData);
+    let data = JSON.parse(localStorage.getItem("stateArray")) || []; 
+
+    data.forEach((countdownData, index) => {
+        setTimeout(() => {
+            createCountdownDisplay(countdownData);
+        }, index * 200)
     });
-    console.log(allData);
 }
 
 // Call the function to load countdowns when the page loads
-window.onload = function () {
+window.onload = () => {
     loadCountdownsFromLocalStorage();
 };
